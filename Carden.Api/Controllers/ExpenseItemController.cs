808b16b6b314438e135435a89270d8398da3e781
Adeyemi Abiade <abiadeabdulazeez@gmail.com>
2025-12-24 15:12:38 +0100

    (feat): Implement expense item business logic

--- Carden.Api/Controllers/ExpenseItemController.cs
+++ Carden.Api/Controllers/ExpenseItemController.cs
@@ -1,6 +1,89 @@
-﻿namespace Carden.Api.Controllers;
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 
-public class ExpenseItemController
+namespace Carden.Api.Controllers;
+
+[ApiController]
+[Route("/api/v1/[controller]")]
+[Authorize]
+public class ExpenseItemController(IExpenseItemService expenseItemService) : ControllerBase
 {
-    
+    private readonly IExpenseItemService _expenseItemService = expenseItemService;
+
+    [HttpPost("add")]
+    public async Task<IActionResult> AddExpenseItem(AddExpenseItemDto dto)
+    {
+        try
+        {
+            var userIdClaim = User.FindFirstValue("sub");
+            if (userIdClaim is null) return Unauthorized(new { Success = false, Message = "unauthorized" });
+
+            var userId = Guid.Parse(userIdClaim);
+            await _expenseItemService.AddExpenseItem(dto, userId);
+            
+            return Created(nameof(AddExpenseItem), new { Success = true, Message = "Item added successfully" });
+        }
+        catch (BadHttpRequestException e)
+        {
+            return BadRequest(new { Success = false, Message = e.Message });
+        }
+    }
+
+    [HttpGet("")]
+    public async Task<IActionResult> GetExpenseItems()
+    {
+        try
+        {
+            var userIdClaim = User.FindFirstValue("sub");
+            if (userIdClaim is null) return Unauthorized(new { Success = false, Message = "unauthorized" });
+
+            var userId = Guid.Parse(userIdClaim);
+            var expenseItems = await _expenseItemService.GetExpenseItems(userId);
+
+            return Ok(new { Success = true, Message = "Items found", Data = new { ExpenseItems = expenseItems } });
+        }
+        catch (BadHttpRequestException e)
+        {
+            return BadRequest((new { Success = false, Message = e.Message }));
+        }
+    }
+
+    [HttpPost("/{expenseItemId:guid}/update-priority")]
+    public async Task<IActionResult> UpdateExpenseItemPriority([FromBody] double priority, Guid expenseItemId)
+    {
+        try
+        {
+            var userIdClaim = User.FindFirstValue("sub");
+            if (userIdClaim is null) return Unauthorized(new { Success = false, Message = "unauthorized" });
+
+            var userId = Guid.Parse(userIdClaim);
+            await _expenseItemService.UpdatePriority(expenseItemId, userId, priority);
+
+            return Ok(new { Success = true, Message = "Item priority updated" });
+        }
+        catch (BadHttpRequestException e)
+        {
+            return BadRequest(new { Success = false, Message = e.Message });
+        }
+    }
+
+    [HttpDelete("/{expenseItemId:guid}/remove")]
+    public async Task<IActionResult> RemoveExpenseItem(Guid expenseItemId)
+    {
+        try
+        {
+            var userIdClaim = User.FindFirstValue("sub");
+            if (userIdClaim is null) return Unauthorized(new { Success = false, Message = "unauthorized" });
+
+            var userId = Guid.Parse(userIdClaim);
+            await _expenseItemService.RemoveExpenseItem(expenseItemId, userId);
+            return Ok(new { Success = true, Message = "Item deleted successfully" });
+        }
+        catch (BadHttpRequestException e)
+        {
+            return BadRequest(new { Success = false, Message = e.Message });
+        }
+    }
 }
+
+public record AddExpenseItemDto(string Name, long Amount, double? Priority = null, string? Description = null);
--- Carden.Api/Services/ExpenseItemService.cs
+++ Carden.Api/Services/ExpenseItemService.cs
@@ -1,6 +1,73 @@
-﻿namespace Carden.Api.Services;
+﻿using Microsoft.EntityFrameworkCore;
 
-public class ExpenseItemService
+namespace Carden.Api.Services;
+
+public interface IExpenseItemService
 {
-    
+    public Task AddExpenseItem(AddExpenseItemDto dto, Guid userId);
+    public Task RemoveExpenseItem(Guid expenseItemId, Guid userId);
+    public Task UpdatePriority(Guid expenseItemId, Guid userId, double priority);
+    public Task<List<ExpenseItem>> GetExpenseItems(Guid userId);
+}
+
+public class ExpenseItemService(AppDbContext context) : IExpenseItemService
+{
+    private readonly AppDbContext _context = context;
+
+    public async Task AddExpenseItem(AddExpenseItemDto dto, Guid userId)
+    {
+        var currentMaxPriority = await _context.ExpenseItems.
+            Where(e => e.UserId == userId).
+            MaxAsync(e => e.Priority);
+        var priority = dto.Priority ?? 0L;
+
+        if (priority > 0 && priority < currentMaxPriority)
+        {
+            // If there is a priority in the dto request body
+            // If the priority is in between other priorities ensure it is given a larger decimal place
+        }
+        else
+        {
+            priority = currentMaxPriority + 1;
+        }
+
+        var expenseItem = new ExpenseItem
+        {
+            Id = Guid.NewGuid(),
+            UserId = userId,
+            Name = dto.Name,
+            Description = dto.Description,
+            Amount = dto.Amount,
+            Priority = priority
+        };
+        await _context.ExpenseItems.AddAsync(expenseItem);
+    }
+
+    public async Task RemoveExpenseItem(Guid expenseItemId, Guid userId)
+    {
+        var expenseItem = await _context.ExpenseItems
+            .FirstOrDefaultAsync(e => e.UserId == userId && e.Id == expenseItemId);
+        if (expenseItem is null) throw new BadHttpRequestException("Item not found");
+
+        _context.ExpenseItems.Remove(expenseItem);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdatePriority(Guid expenseItemId, Guid userId, double priority)
+    {
+        var expenseItem = await _context.ExpenseItems
+            .FirstOrDefaultAsync(e => e.UserId == userId && e.Id == expenseItemId);
+        if (expenseItem is null) throw new BadHttpRequestException("Item not found");
+
+        expenseItem.Priority = priority;
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task<List<ExpenseItem>> GetExpenseItems(Guid userId)
+    {
+        return await _context.ExpenseItems
+            .Where(e => e.UserId == userId)
+            .OrderBy(e => e.Priority)
+            .ToListAsync();
+    }
 }
